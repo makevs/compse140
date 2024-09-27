2ba8a91b33c14af4e2b29bda145e9fce60b4d9b7
makevs <make@texas.fail>
2024-09-27 17:02:17 +0300

    Added a utility class for generating the http response

---
+++ service2/service2/ResponseGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace service2;
+
+public class ResponseGenerator
+{
+    public string Response => _response;
+    private string? _response = "";
+
+    public void GenerateResponse()
+    {
+        string pid = System.Environment.ProcessId.ToString();
+        _response = $"Service 2: {pid}";
+    }
+}
