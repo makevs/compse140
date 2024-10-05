257ea31ed1e546c4512c512fd67bc65974fe84eb
makevs <make@texas.fail>
2024-10-05 11:07:53 +0300

    Remove redundant testcode

--- service1/src/index.ts
+++ service1/src/index.ts
@@ -5,11 +5,6 @@ const sysInfo = getSystemInfo();
 
 const app = new Elysia().get("/", () => sysInfo).listen(8199);
 const message:string = `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-const message_test:string = `- ip address information\n
-  - ip address: ${app.server?.hostname}\n
-  - list of running processes: ${app.server?.port}\n
-  - available disk space: ${app.server?.hostname}\n
-  - time since last boot: ${app.server?.port}\n`;
 
 console.log(
   message  
